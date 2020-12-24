d8025677840899a77a16e1774978be2fbbc5cbbe
unknown <marie1st7880@gmail.com>
2020-12-24 15:29:13 +0700

    DELETE employee_id to hasMany

--- src/models/customers.model.ts
+++ src/models/customers.model.ts
@@ -12,60 +12,106 @@ export class Customers extends Entity {
 
   @property({
     type: 'string',
+    length: 50,
     required: true,
   })
   first_name: string;
 
   @property({
     type: 'string',
+    length: 50,
     required: true,
   })
   last_name: string;
 
   @property({
     type: 'string',
+    length: 13,
     required: true,
   })
   phone_number: string;
 
   @property({
     type: 'string',
-  })
-  date_of_birth: string;
-
-  @property({
-    type: 'string',
-  })
-  career?: string;
-
-  @property({
-    type: 'string',
-    required: true,
-  })
-  pid: string;
-
-  @property({
-    type: 'string',
+    length: 50,
   })
   id_line?: string;
 
   @property({
     type: 'string',
+    length: 50,
+  })
+  occupation?: string;
+
+  @property({
+    type: 'string',
+    length: 100,
     required: true,
   })
   email: string;
 
   @property({
     type: 'string',
+    length: 100,
   })
   facebook?: string;
 
   @property({
-    type: 'number',
+    type: 'string',
+    length: 100,
   })
-  usersId?: number;
+  profile_pic?: string;
 
-  @hasMany(() => Orders)
+  @property({
+    type: 'string',
+    length: 100,
+  })
+  id_card_pic: string;
+
+  @property({
+    type: 'string',
+    length: 100,
+  })
+  address?: string;
+
+  @property({
+    type: 'string',
+    length: 50,
+  })
+  country: string;
+
+  @property({
+    type: 'string',
+    length: 50,
+  })
+  province: string;
+
+  @property({
+    type: 'string',
+    length: 50,
+  })
+  subdistrict: string;
+
+  @property({
+    type: 'string',
+    length: 50,
+  })
+  district: string;
+
+  @property({
+    type: 'string',
+    length: 5,
+  })
+  postcode: string;
+
+  @property ({
+    type: 'date',
+    dataType: 'timestamp',
+    defaultFn: 'now',
+  })
+  created_at: Date;
+
+  @hasMany(() => Orders, {keyFrom:'id'})
   orders: Orders[];
 
   constructor(data?: Partial<Customers>) {
